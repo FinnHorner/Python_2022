bc083092eea45b65298ac2515f37ccfe1b6d4041
FinnHorner <finhor35@kingshigh.school.nz>
2022-02-28 13:24:59 +1300

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -8,3 +8,10 @@ print("hello world" + 24)
 print("hello world" - 24)
 print("hello world" / 5)
 """
+
+str(55+65)
+int(55.0)
+float(44+10)
+print("I was born in the U.S.A. \nI was born in the U.S.A.\n Born in the U.S.A.") #1
+print("This is where the fun begins.") #2
+
