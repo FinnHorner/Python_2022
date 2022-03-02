8ccaaf8ad98d886f9b6452bc71c546bb81d0e758
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-03 09:55:32 +1300

    variables

---
+++ Variables1.py
@@ -0,0 +1,20 @@
+player_lives=9#1
+player_name="Riley"#2
+player_money=100,000#3
+
+print("\n")
+phone_number = input("what is thy phone number madam?")#4
+user_name = input("what is thy name?")#5
+user_age = input("what is thy age?")#6
+year = input("what is thy year")#7
+print(f"your phone is {phone_number}")
+print(f"your name is {user_name}")#8
+print(f"you are {user_age}")#9
+print(f"the year is {year}")#10
+
+print("\n")
+users_surname = input("what is thy surname?")#11
+users_school = input("what school do you go to?")#12
+users_fav_food = input("what is your fav food?")#13
+users_pet = input("what is your pets name")#14
+
