8e1c9a20b0058f85f6c54ab868ee85658d8f9fb0
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-04 10:59:42 +1300

    riley

--- Variables1.py
+++ Variables1.py
@@ -7,7 +7,6 @@ phone_number = input("what is thy phone number madam?")#4
 user_name = input("what is thy name?")#5
 user_age = input("what is thy age?")#6
 year = input("what is thy year")#7
-print(f"your phone is {phone_number}")
 print(f"your name is {user_name}")#8
 print(f"you are {user_age}")#9
 print(f"the year is {year}")#10
@@ -17,4 +16,19 @@ users_surname = input("what is thy surname?")#11
 users_school = input("what school do you go to?")#12
 users_fav_food = input("what is your fav food?")#13
 users_pet = input("what is your pets name")#14
+
+print("\n")
+blocks_of_chocolate = 5 #15
+players_score = 100 #16
+players_highscore = 200 #17
+track = 3 #18
+ammunition_left = 60 #19
+current_gear = 2 #20
+current_speed = 243 #21
+
+print("\n")
+
+
+
+
 
---
+++ exercie_variables.py
@@ -0,0 +1,13 @@
+money = 200
+hat = 20
+top = 30
+pants =15
+belt = 60
+shoes = 40
+print(f"you have {money}")
+print("Buying hat")
+print("you now have the following amount left over")
+print(money-hat)
+
+print("\n")
+
