64ef1e7aaf30dba5e7193e7fd8ed4fa499e011ad
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-08 13:24:54 +1300

    Update exercie.py

--- exercie.py
+++ exercie.py
@@ -1,3 +1,4 @@
+"""
 input("push any key then push enter") #1
 input("type your name and push enter")#2
 input("type age and push enter") #3
@@ -9,8 +10,41 @@ book=input("what is your favourite book?")#7
 adjective=input("give me an adjective")#8
 noun=input("give me a noun")#9
 verb=input("give me a verb")#10
+#11
 print(f"hello {name}")
 print(f"you are {age} yaers old")
 print(f"your favourite moive is {movie}")
 print(f"your favourite book is {book}")
-print(f"you gave me the adjective {adjective")
+print(f"you gave me the adjective {adjective}")
+print(f"you gave me the noun {noun}")
+print(f"you gave me the verb {verb}")
+
+print("\n")
+user_age = int(input("How old are you?")) #12
+print(f"You will be {user_age+10} in 10 years.")#13
+print(f"You where born in {2022-user_age}")#14
+users_apples=int(input("How many apples do you have?"))#15
+users_friends=int(input("How many friends do you have?"))#16
+print(f"Each friend will get {users_apples/users_friends} each.")#17
+
+"""
+users_pizzas = int(input("How many pizzas do you want?"))#18
+how_many_feeding = int(input("How many are you feeding?"))#19
+print(f"each person will get {users_pizzas*8/how_many_feeding} slices of pizza.")#20
+dollars = int(input("How many dollars do you have??"))#21
+TV_cost = int(input("How much does a TV cost?"))#22
+print(f"you will have {dollars-TV_cost} left.")#23
+print(f"if you wait for the discount the TV will cost {TV_cost*0.8}.")#24
+
+
+users_bitcoins = float(input("How many bitcoins do you have? "))#25
+
+
+
+
+
+
+
+
+
+
