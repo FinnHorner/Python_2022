d5b86baa2996a89e0b1cf01526d9131bee51e09f
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-11 09:54:38 +1300

    heheh

--- conditionals.py
+++ conditionals.py
@@ -10,7 +10,7 @@ if how_far >200:
 	print("Make sure to fill up with petrol!")
 elif how_far <200:
 	print("good Luck!")
-"""
+
 users_age = int(input("What is your age")) #3
 if users_age >=18:
 	print("You are now a adult")
@@ -18,3 +18,22 @@ elif users_age <18:
 	print("you are a child")
 
 
+fav_movie = input("what is your favourite movie?") #4
+if fav_movie =="Lord of the rings":
+	print("you have excellent taste")
+else:
+	print("lord of the rings is better.")
+
+
+
+darth_plagueis = input("Have you heard the tragedy of darth plagueis the wise?") #5
+if darth_plagueis == "yes":
+	print("You must be a fan!")
+else:
+	print("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
+"""
+passion_of_christ = input("Who directed Passion of the Christ?")
+if passion_of_christ == "Mel Gibson":
+	print("Correct!")
+else:
+	print("wrong!")
