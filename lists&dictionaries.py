99cea66a6fd5c08be34547d6f1d8167f66fad980
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-14 11:00:07 +1300

    erickim

---
+++ lists&dictionaries.py
@@ -0,0 +1,6 @@
+fibonacci=(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)#1
+print(fibonacci)
+fruits=("Apple, pear, orange, tomatoe, eric kim")#2
+print(fruits)
+youtubers=("Deji, dude pefect, ali-a, ksi, tucker gott, electro boom, hacksmith industires.")#3
+print(youtubers)
