eb902744f0301cc030e240cf46542bea2398f26f
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-15 12:24:54 +1300

    Update erickim.py

--- erickim.py
+++ erickim.py
@@ -1,6 +1,49 @@
+"""
 fibonacci=(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)#1
 print(fibonacci)
 fruits=("Apple, pear, orange, tomatoe, eric kim")#2
 print(fruits)
 youtubers=("Deji, dude pefect, ali-a, ksi, tucker gott, electro boom, hacksmith industires.")#3
 print(youtubers)
+music=[]
+music.append("carzy frog") #4
+music.append("I like to move it move it.")
+music.append("Peanut butter jelly time.")
+music.append("What does the fox say")
+music.append("Fishy on me")
+print(','. join(music))
+print("\n")
+
+books=[]
+books.append("Rileys biography") #5
+books.append("Harry Potter")
+books.append("why K-POP sucks")
+books.append("fortnite tactics")
+books.append("fortnite 100 tips and tricks")
+books.append("why i dislike eric kim")
+print(','. join(books))
+print("\n")
+"""
+pizza_toppings=[]
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza=input("add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+print(pizza_toppings)
+
+
+
