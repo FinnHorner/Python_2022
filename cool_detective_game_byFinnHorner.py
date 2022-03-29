d9af9766894fb5d88aff01d035479e9f91d1a817
FinnHorner <finhor35@kingshigh.school.nz>
2022-03-30 09:59:48 +1300

    Update cool_detective_game_byFinnHorner.py

---
+++ cool_detective_game_byFinnHorner.py
@@ -0,0 +1,19 @@
+from adventurelib import *
+
+#Room description 
+car_park = Room("""
+	This is where your investagation begins, You find yourself at a muder sence. 
+	a old man has been stabed 57 times in the chest, the body is located in an ally way behind the old pub. You know nothing yet. 
+	""")
+
+house_1 = Room(""" 
+	A young couple live here. Its the house accross the street from the pub. they tell you they herad talking but wernt able to make out.
+	They have no other information. """)
+
+house_2 = Room("""
+	A older couple live here, they are right next door to the alyway where the man was murdered. They tell you they heard a loud thump hiting the pavment. They did also here talking but couldnt make it out because they where making tea.
+	""")
+
+Leve1_pub = Room("""
+	Its quite here mostly because everybody is looking at the body outside. The bartender is presnt though, she didnt see or hear anything but suggesets you talk the people upstairs, they may of heard or saw something. 
+	""")
